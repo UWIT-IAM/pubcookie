aeb4abc9ebe1f2cab612a5d758035ead47990509
Steve Mats Mats <willey@uw.edu>
1998-06-25 01:05:38 +0000

    made chars unsigned, and some other crap
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@7 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pubcookie.h
+++ src/pubcookie.h
@@ -1,29 +1,30 @@
 /*
-    $Id: pubcookie.h,v 1.1 1998-06-24 18:14:50 willey Exp $
+    $Id: pubcookie.h,v 1.2 1998-06-25 01:05:38 willey Exp $
  */
 
 #ifndef PUBCOOKIE_MAIN
 #define PUBCOOKIE_MAIN
 
 #define PBC_USER_LEN 42
-#define PBC_VER_LEN 2
+#define PBC_VER_LEN 4
 #define PBC_APPSRV_ID_LEN 20
 #define PBC_APP_ID_LEN 10
 
 typedef struct {
-    char	user[PBC_USER_LEN];
-    char	version[PBC_VER_LEN];
-    char	type;
-    time_t	create_ts;
-    time_t	last_ts;
-    char	creds;
-    char	appsrv_id[PBC_APPSRV_ID_LEN];
-    char	app_id[PBC_APP_ID_LEN];
+    unsigned char	user[PBC_USER_LEN];
+    unsigned char	version[PBC_VER_LEN];
+    unsigned char	type;
+    time_t		create_ts;
+    time_t		last_ts;
+    unsigned char	creds;
+    unsigned char	appsrv_id[PBC_APPSRV_ID_LEN];
+    unsigned char	app_id[PBC_APP_ID_LEN];
 } pbc_cookie_data;
 
 typedef struct {
     EVP_MD_CTX	*ctx;
     EVP_PKEY 	*private_key;
+    EVP_PKEY 	*public_key;
 } context_plus;
 
 #endif /* !PUBCOOKIE_MAIN */
