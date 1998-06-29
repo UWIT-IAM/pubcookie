a74ed7d77fde87baa1f65ae2d656f97ab96e6868
Steve Mats Mats <willey@uw.edu>
1998-06-29 22:23:16 +0000

    regular checking
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@11 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/libpubcookie.c
+++ src/libpubcookie.c
@@ -1,5 +1,5 @@
 /*
-    $Id: libpubcookie.c,v 1.2 1998-06-25 03:00:58 willey Exp $
+    $Id: libpubcookie.c,v 1.3 1998-06-29 22:23:16 willey Exp $
  */
 
 #include <stdio.h>
@@ -44,21 +44,21 @@ int libpbc_debug(const char *format,...)
     return 0;
 }
 
-//char *libpbc_sign_cookie(har *cookie_string, pbc_context_plus *ctx_plus)
-//{}
+unsigned char *libpbc_sign_cookie(unsigned char *cookie_string, context_plus *ctx_plus)
 /*                                                                            */
-//char *libpbc_sign_cookie(pbc_context_plus *ctx_plus) 
-//char *libpbc_sign_cookie(char *cookie_string, pbc_context_plus *ctx_plus) 
-//{
-    //char	*sig;
-    //int		*sig_len;
-//
-    //EVP_SignUpdate(ctx_plus->ctx, cookie_string, strlen(cookie_string));
-    //if( EVP_SignFinal(ctx_plus->ctx, sig, sig_len, ctx_plus->private_key) )
-	//return sig;
-    //else
-	//return NULL;
-//}
+{
+    unsigned char	*sig;
+    unsigned int	sig_len = 0;
+
+    sig = pbc_malloc(4096);
+    memset(sig, 0, sizeof(sig));
+
+    EVP_SignUpdate(ctx_plus->ctx, cookie_string, strlen(cookie_string));
+    if( EVP_SignFinal(ctx_plus->ctx, sig, &sig_len, ctx_plus->private_key) )
+	return sig;
+    else
+	return NULL;
+}
 
 /*                                                                            */
 int libpbc_get_private_key(context_plus *ctx_plus) 
@@ -103,13 +103,15 @@ int libpbc_get_public_key(context_plus *ctx_plus)
     return 1;
 }
 
-/* mallocs and initializes a pbc_cookie_data struct                           */
+/* mallocs a pbc_cookie_data struct                                           */
 pbc_cookie_data *libpbc_init_cookie_data()
 {
     pbc_cookie_data *cookie_data;
 
-    if( (cookie_data=(pbc_cookie_data *)pbc_malloc(sizeof(pbc_cookie_data))) )
-        memset(cookie_data, 0, sizeof(cookie_data));
+    if( ! (cookie_data=(pbc_cookie_data *)pbc_malloc(sizeof(pbc_cookie_data))) )
+	libpbc_abend("libpbc_init_cookie_data: failed to allocate memory\n");
+
+    memset(cookie_data, ' ', sizeof(pbc_cookie_data));
     return cookie_data;
 }
 
@@ -131,7 +133,45 @@ context_plus *libpbc_init_context_plus()
     return ctx_plus;
 }
 
+/* check a signature after context is established                             */
+int libpbc_verify_sig(unsigned char *sig, unsigned char *cookie_string, context_plus *ctx_plus)
+{
+
+    EVP_VerifyUpdate(ctx_plus->ctx, cookie_string, strlen(cookie_string));
+    return EVP_VerifyFinal(ctx_plus->ctx, sig, PBC_SIG_LEN, ctx_plus->public_key);
+}
+
+/*                                                                            */
 /* get public key and initialize verify context                               */
+/*                                                                            */
+unsigned char *libpbc_stringify_cookie_data(pbc_cookie_data *cookie_data) 
+{
+    unsigned char	*cookie_string;
+    unsigned char	*p;
+
+    p = cookie_string = pbc_malloc(sizeof(pbc_cookie_data)+1);
+
+//    memcpy(cookie_string, (*cookie_data).string, sizeof(pbc_cookie_data));
+    memcpy(p, (*cookie_data).broken.user, PBC_USER_LEN);
+    p = p + PBC_USER_LEN-1;
+    *p = ' ';
+    p++;
+    memcpy(p, (*cookie_data).broken.version, PBC_VER_LEN));
+    p = p + PBC_VER_LEN-1;
+    *p = ' ';
+    p++;
+    cookie_string[PBC_USER_LEN+PBC_VER_LEN+PBC_APPSRV_ID_LEN-3] = ' ';
+    cookie_string[PBC_USER_LEN+PBC_VER_LEN+
+                  PBC_APPSRV_ID_LEN+PBC_APP_ID_LEN-4] = ' ';
+
+    cookie_string[sizeof(pbc_cookie_data)+1] = '\0';
+    return cookie_string;
+
+}
+
+/*                                                                            */
+/* get public key and initialize verify context                               */
+/*                                                                            */
 context_plus *libpbc_verify_init() 
 {
     context_plus *ctx_plus;
@@ -143,7 +183,9 @@ context_plus *libpbc_verify_init()
     return ctx_plus;
 }
 
+/*                                                                            */
 /* get private key and initialize context                                     */
+/*                                                                            */
 context_plus *libpbc_sign_init() 
 {
     context_plus *ctx_plus;
@@ -154,85 +196,41 @@ context_plus *libpbc_sign_init()
     return ctx_plus;
 }
 
-/* check a signature after context is established                             */
-int libpbc_verify_sig(char *sig, char *cookie_string, context_plus *ctx_plus)
-{
-
-    EVP_VerifyUpdate(ctx_plus->ctx, cookie_string, strlen(cookie_string));
-    return EVP_VerifyFinal(ctx_plus->ctx, sig, PBC_SIG_LEN, ctx_plus->public_key);
-}
-
-/* convert cookie_data structure to a char string                             */
-char *libpbc_stringify_cookie_data(pbc_cookie_data *cookie_data)
-{
-    char	*cookie_string;
-    char	*p;
-    int		len;
-
-    len =(strlen(cookie_data->user) +
-          strlen(cookie_data->version) +
-          1 +					// cookie_data->type
-          1 +					// cookie_data->creds
-//          strlen(cookie_data->create_ts) +
-//          strlen(cookie_data->last_ts) +
-          strlen(cookie_data->appsrv_id) +
-          strlen(cookie_data->app_id) +1) * sizeof(char);
-    cookie_string = pbc_malloc( len );
-    strcat(cookie_string, cookie_data->user);
-    strcat(cookie_string, cookie_data->version);
-
-    p = strchr(cookie_string, '\0');
-    *p = cookie_data->type;
-    *p++ = cookie_data->creds;
-    *p++ = '\0';
-//    strcat(cookie_string, cookie_data->create_ts);
-//    strcat(cookie_string, cookie_data->last_ts);
-    strcat(cookie_string, cookie_data->appsrv_id);
-    strcat(cookie_string, cookie_data->app_id);
-
-    return cookie_string;
-}
-
-/* convert a cookie char string into a cookie_data structure                  */
-pbc_cookie_data *libpbc_destringify_cookie_data(char *cookie_string)
-{
-   
-}
-
 /*                                                                            */
 /* builds, signs and returns cookie                                           */
 /*                                                                            */
-char *libpbc_get_cookie(char *user, 
-	                  char type, 
-			  char creds,
-			  char *appsrv_id,
-			  char *app_id,
+unsigned char *libpbc_get_cookie(char *user, 
+	                  unsigned char type, 
+			  unsigned char creds,
+			  unsigned char *appsrv_id,
+			  unsigned char *app_id,
 			  context_plus *ctx_plus) 
 {
 
-    pbc_cookie_data 	*cookie_data;
-    char		*cookie_string;
-    char		*cookie;
-    char		*sig;
+    pbc_cookie_data 		*cookie_data;
+    unsigned char		*cookie;
+    unsigned char		*cookie_string;
+    unsigned char		*sig;
 
     cookie_data = libpbc_init_cookie_data();
 
-    strncpy(cookie_data->user, user, PBC_USER_LEN);
-    strncpy(cookie_data->version, PBC_VERSION, PBC_VER_LEN);
-    cookie_data->type = type;
-    cookie_data->creds = creds;
-    cookie_data->create_ts = time(NULL);
-    cookie_data->last_ts = time(NULL);
-    strncpy(cookie_data->appsrv_id, appsrv_id, PBC_APPSRV_ID_LEN);
-    strncpy(cookie_data->app_id, app_id, PBC_APP_ID_LEN);
+    strncpy((*cookie_data).broken.user, (unsigned char *)user, PBC_USER_LEN);
+    strncpy((*cookie_data).broken.version, PBC_VERSION, PBC_VER_LEN);
+    (*cookie_data).broken.type = type;
+    (*cookie_data).broken.creds = creds;
+    (*cookie_data).broken.create_ts = time(NULL);
+    (*cookie_data).broken.last_ts = time(NULL);
+    strncpy((*cookie_data).broken.appsrv_id, appsrv_id, PBC_APPSRV_ID_LEN);
+    strncpy((*cookie_data).broken.app_id, app_id, PBC_APP_ID_LEN);
   
-    // make cookie string
+    // sign cookie
     cookie_string = libpbc_stringify_cookie_data(cookie_data);
 
-    // sign cookie
-//    sig = libpbc_sign_cookie(cookie_string, ctx_plus);
+    sig = libpbc_sign_cookie(cookie_string, ctx_plus);
 
-    cookie = strcat(strcat(cookie, sig), cookie_string);
+    cookie = pbc_malloc( (PBC_SIG_LEN+strlen((*cookie_data).string)+1) );
+    cookie = (unsigned char *)strncpy(cookie, sig, PBC_SIG_LEN);
+    cookie = (unsigned char *)strcat(cookie, (*cookie_data).string);
 
     // encrypt cookie
 
@@ -245,29 +243,62 @@ char *libpbc_get_cookie(char *user,
 pbc_cookie_data *libpbc_unbundle_cookie(char *cookie, context_plus *ctx_plus) 
 {
     pbc_cookie_data	*cookie_data;
-    char		*sig;
-    char		*cookie_string;
+    unsigned char		*sig;
+    unsigned char		*cookie_string;
 
-    if ( strlen(cookie) < PBC_SIG_LEN ) {
+    if ( strlen(cookie) < (PBC_SIG_LEN) ) {
 	libpbc_debug("libpbc_unbundle_cookie: cookie %s too short\n", cookie);
 	return 0;
     }
 
     /* break cookie in two */
-    sig = pbc_strndup(cookie, PBC_SIG_LEN);
-    cookie_string = pbc_strndup(cookie+PBC_SIG_LEN, strlen(cookie)-PBC_SIG_LEN+1);
+    sig = (unsigned char *)pbc_strndup(cookie, PBC_SIG_LEN);
+    cookie_string = (unsigned char *)pbc_strndup(cookie+PBC_SIG_LEN, strlen(cookie)-PBC_SIG_LEN+1);
     // decrypt cookie
 
     cookie_data = libpbc_init_cookie_data();
 
     if( (libpbc_verify_sig(sig, cookie_string, ctx_plus)) ) {
-        cookie_data = libpbc_destringify_cookie_data(cookie_string);
+        strcpy((*cookie_data).string, cookie_string);
         return cookie_data;
     }
     else {
         return NULL;
     }
 
+
+}
+    
+/*                                                                            */
+/*  update last_ts in cookie                                                  */
+/*                                                                            */
+unsigned char *libpbc_update_lastts(char *cookie, context_plus *ctx_plus) 
+{
+    pbc_cookie_data	*cookie_data;
+    unsigned char	*sig;
+    unsigned char	*cookie_string;
+
+    if ( strlen(cookie) < (PBC_SIG_LEN) ) {
+	libpbc_debug("libpbc_unbundle_cookie: cookie %s too short\n", cookie);
+	return 0;
+    }
+
+    // decrypt cookie
+
+    /* break cookie in two */
+    sig = (unsigned char *)pbc_strndup(cookie, PBC_SIG_LEN);
+    cookie_string = (unsigned char *)pbc_strndup(cookie+PBC_SIG_LEN, strlen(cookie)-PBC_SIG_LEN+1);
+
+    cookie_data = libpbc_init_cookie_data();
+
+    if( (libpbc_verify_sig(sig, cookie_string, ctx_plus)) ) {
+        strcpy((*cookie_data).string, cookie_string);
+    }
+    else {
+        return cookie_string;
+    }
+
+    return cookie_string;
 
 }
     
