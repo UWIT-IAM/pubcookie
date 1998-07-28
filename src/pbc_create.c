428d9c4c446408cc8982c2357e981605ee8b0587
Steve Mats Mats <willey@uw.edu>
1998-07-28 23:03:58 +0000

    modified to accept more args on command line
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@25 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_create.c
+++ src/pbc_create.c
@@ -1,6 +1,13 @@
 /*
-    $Id: pbc_create.c,v 1.4 1998-07-24 23:14:00 willey Exp $
+    $Id: pbc_create.c,v 1.5 1998-07-28 23:03:57 willey Exp $
  */
+
+/* this is not meant to be user friendly, no friendlyness for anyone          */
+/*   but me and i have the src code                                           */
+/*                                                                            */
+/* args are: user appsrv_id app_id type creds                                 */
+/*    (anything too big is just truncated)                                    */
+/*                                                                            */
 
 #include <stdio.h>
 #include <time.h>
@@ -11,40 +18,44 @@
 #include "pbc_config.h"
 
 int main(int argc, char **argv) {
-    md_context_plus *ctx_plus;
-    unsigned char type;
-    unsigned char creds;
-    unsigned char appsrv_id[PBC_APPSRV_ID_LEN];
-    unsigned char app_id[PBC_APP_ID_LEN];
-    unsigned char *cookie;
+    md_context_plus 	*ctx_plus;
     crypt_stuff         *c_stuff;
-    FILE		*fp;
 
-    type='1';
-    creds='9';
-    strcpy(appsrv_id, "appserver id is blah");
-    strcpy(app_id, "application id is googoo");
+    unsigned char 	user[PBC_USER_LEN];
+    unsigned char 	appsrv_id[PBC_APPSRV_ID_LEN];
+    unsigned char 	app_id[PBC_APP_ID_LEN];
+    unsigned char 	type;
+    unsigned char 	creds;
 
-    if(argc != 2) {
-        fprintf(stderr, "usage: %s username\n", argv[0]);
+    unsigned char 	*cookie;
+
+    if(argc != 6)
         exit(1);
-    }
 
-    if(strlen(argv[1]) > 256) {
-        fprintf(stderr, "Username is too long.\n");
+    strncpy(user, argv[1], sizeof(user));
+    user[sizeof(user)-1] = '\0';
+    strncpy(appsrv_id, argv[2], sizeof(appsrv_id));
+    appsrv_id[sizeof(appsrv_id)-1] = '\0';
+    strncpy(app_id, argv[3], sizeof(app_id));
+    appsrv_id[sizeof(app_id)-1] = '\0';
+    type = argv[4][0];
+    creds = argv[5][0];
+
+    if ( type == PBC_COOKIE_TYPE_G ) 
+        ctx_plus = libpbc_sign_init(PBC_G_KEYFILE);
+    else if ( type == PBC_COOKIE_TYPE_L ) 
+        ctx_plus = libpbc_sign_init(PBC_L_KEYFILE);
+    else if ( type == PBC_COOKIE_TYPE_S ) 
+        ctx_plus = libpbc_sign_init(PBC_S_KEYFILE);
+    else
         exit(1);
-    }
 
-    ctx_plus = libpbc_sign_init(PBC_G_KEYFILE);
     c_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
-    cookie = libpbc_get_cookie(argv[1], type, creds, appsrv_id, app_id, ctx_plus, c_stuff);
 
-    fp = fopen("out", "w");
-    if ( cookie ) {
-	fprintf(fp, "%s", cookie);
-	printf("%s\n", cookie);
-    }
-    fclose(fp);
+    cookie = libpbc_get_cookie(user, type, creds, appsrv_id, app_id, ctx_plus, c_stuff);
+
+    printf("%s", cookie);
+    
     exit(0);
 
 }
--- src/pbc_verify.c
+++ src/pbc_verify.c
@@ -1,6 +1,10 @@
 /*
-    $Id: pbc_verify.c,v 1.4 1998-07-24 23:14:00 willey Exp $
+    $Id: pbc_verify.c,v 1.5 1998-07-28 23:03:57 willey Exp $
  */
+
+/*                                                                            */
+/* args are: type                                                             */
+/*                                                                            */
 
 #include <stdio.h>
 #include <time.h>
@@ -11,28 +15,38 @@
 #include "libpubcookie.h"
 
 int main(int argc, char **argv) {
-    char 		buf[PBC_4K];
     md_context_plus	*ctx_plus;
-    pbc_cookie_data	*cookie_data;
-    FILE		*fp;
-    char		in[PBC_4K];
     crypt_stuff         *c_stuff;
+    pbc_cookie_data	*cookie_data;
+    char		in[PBC_4K];
+    unsigned char       type;
 
-    memset(buf, 0, sizeof(buf));
+    fgets(in, sizeof(in), stdin);
 
-    fp = fopen("out", "r");
-    fgets(in, sizeof(in), fp);
-    fclose(fp);
+    type = argv[1][0];
+    if ( type == PBC_COOKIE_TYPE_G )
+        ctx_plus = libpbc_verify_init(PBC_G_CERTFILE);
+    else if ( type == PBC_COOKIE_TYPE_L )
+        ctx_plus = libpbc_verify_init(PBC_L_CERTFILE);
+    else if ( type == PBC_COOKIE_TYPE_S )
+        ctx_plus = libpbc_verify_init(PBC_S_CERTFILE);
+    else
+	exit (1);
 
-    ctx_plus = libpbc_verify_init(PBC_G_CERTFILE);
     c_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
-    if( ! (cookie_data = libpbc_unbundle_cookie(in, ctx_plus, c_stuff)) ) {
-	fprintf(stderr, "Could not verify signature.\n");
-	exit(1);
-    }
 
-    printf("user is >%s<\n", (*cookie_data).broken.user);
-    printf("app_id is >%s<\n", (*cookie_data).broken.app_id);
+    if( ! (cookie_data = libpbc_unbundle_cookie(in, ctx_plus, c_stuff)) )
+	exit(1);
+
+    printf("user: %s\n", (*cookie_data).broken.user);
+    printf("version: %s\n", (*cookie_data).broken.version);
+    printf("type: %c\n", (*cookie_data).broken.type);
+    printf("creds: %c\n", (*cookie_data).broken.creds);
+    printf("appsrv_id: %s\n", (*cookie_data).broken.appsrv_id);
+    printf("app_id: %s\n", (*cookie_data).broken.app_id);
+    printf("create_ts: %d\n", (int)(*cookie_data).broken.create_ts);
+    printf("last_ts: %d\n", (int)(*cookie_data).broken.last_ts);
+    
     exit(0);
 
 }
