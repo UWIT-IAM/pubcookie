3e4307d73b83bbc35a5a12ee49dcb1c00b9a3caa
Steve Mats Mats <willey@uw.edu>
1998-07-28 23:03:59 +0000

    misc modifications
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@26 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_version.h
+++ src/pbc_version.h
@@ -1,10 +1,10 @@
 /*
-    $Id: pbc_version.h,v 1.2 1998-07-20 10:34:34 willey Exp $
+    $Id: pbc_version.h,v 1.3 1998-07-28 23:03:57 willey Exp $
  */
 
 #ifndef PUBCOOKIE_VERSION
 #define PUBCOOKIE_VERSION
 
-#define PBC_VERSION "a1"
+#define PBC_VERSION "a2"
 
 #endif /* !PUBCOOKIE_VERSION */
