82fe2ec5361b456fdbd8b6b31a241b3f9a717ec0
Steve Mats Mats <willey@uw.edu>
1999-01-04 22:09:13 +0000

    now takes the ip number as an argument
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@41 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/mkc_key_local.c
+++ src/mkc_key_local.c
@@ -10,16 +10,20 @@
 #include "pbc_config.h"
 #include "pbc_version.h"
 
-void main() {
+int main(int argc, char **argv) {
     unsigned char 	*key_buf;
     unsigned char       *addr;
+    FILE		*fp;
 
     key_buf = (unsigned char *)libpbc_alloc_init(PBC_DES_KEY_BUF);
 
-    if( fread(key_buf, sizeof(char), PBC_DES_KEY_BUF, stdin) != PBC_DES_KEY_BUF)
-        libpbc_abend("libpbc_crypt_key: Failed read\n");
+    if( ! (fp = pbc_fopen(PBC_CRYPT_KEYFILE, "r")) )
+        libpbc_abend("make localized crypt key: Failed open\n");
 
-    addr = libpbc_gethostip();
+    if( fread(key_buf, sizeof(char), PBC_DES_KEY_BUF, fp) != PBC_DES_KEY_BUF)
+        libpbc_abend("make localized crypt key: Failed read\n");
+
+    addr = argv[1];
     key_buf = libpbc_mod_crypt_key(key_buf, addr);
     
     if( fwrite(key_buf, sizeof(char), PBC_DES_KEY_BUF, stdout) != PBC_DES_KEY_BUF)
