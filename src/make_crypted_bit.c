e72b8bee61e75d4e58bf3f34681b7c1dff6b2f4c
Steve Mats Mats <willey@uw.edu>
1999-01-06 05:16:34 +0000

    Initial revision
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@42 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

---
+++ src/make_crypted_bit.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pem.h>
+#include "pubcookie.h"
+#include "libpubcookie.h"
+#include "pbc_config.h"
+
+void *main() {
+    crypt_stuff		*c1_stuff;
+    unsigned char	in[PBC_1K];
+    unsigned char	intermediate[PBC_1K];
+
+    c1_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
+
+    bzero(in, 1024);
+    bzero(intermediate, 1024);
+    strcpy(in, "fasdfasdfsadfak2eiojslkdjf2io3erjlskdfjsdalkj asdfdf");
+
+    fprintf(stderr, "in is %s\n", in);
+    if ( ! libpbc_encrypt_cookie(in, intermediate, c1_stuff, strlen(in)) )
+        exit(0);
+    printf("%s", intermediate); 
+
+    exit(1);
+
+}
---
+++ src/test_local_c_key.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pem.h>
+#include "pubcookie.h"
+#include "libpubcookie.h"
+#include "pbc_config.h"
+
+void *main() {
+    crypt_stuff		*c1_stuff;
+    unsigned char	in[PBC_1K];
+    unsigned char	intermediate[PBC_1K];
+    unsigned char	out[PBC_1K];
+
+    c1_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
+
+    bzero(in, 1024);
+    bzero(out, 1024);
+    bzero(intermediate, 1024);
+    strcpy(in, "fasdfasdfsadfak2eiojslkdjf2io3erjlskdfjsdalkj asdfdf");
+
+    fread(intermediate, sizeof(char), PBC_1K, stdin);
+
+    if ( ! libpbc_decrypt_cookie(intermediate, out, c1_stuff, strlen(in)) )
+        exit(0);
+    printf("out is %s\n", out);
+
+    if( memcmp(in,out,sizeof(in)) != 0 )
+	printf("cfb64 encrypt/decrypt error\n");
+    else
+	printf("it worked\n");
+
+    exit(1);
+
+}
