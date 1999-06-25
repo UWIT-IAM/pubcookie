6ed8ea762820c241c01dc9279ac4fe580f423418
Steve Mats Mats <willey@uw.edu>
1999-06-25 22:14:33 +0000

    a5 rebook2
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@138 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_version.h
+++ src/pbc_version.h
@@ -1,11 +1,11 @@
 /*
-    $Id: pbc_version.h,v 1.9 1999-05-26 18:00:24 willey Exp $
+    $Id: pbc_version.h,v 1.10 1999-06-25 22:14:33 willey Exp $
  */
 
 #ifndef PUBCOOKIE_VERSION
 #define PUBCOOKIE_VERSION
 
-#define PBC_VERSION "a4"
-#define PBC_TESTID "reebok1"
+#define PBC_VERSION "a5"
+#define PBC_TESTID "rebook2"
 
 #endif /* !PUBCOOKIE_VERSION */
