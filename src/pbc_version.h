b3dc1abfaaf25eca8f371f556454309f61feceb0
Steve Mats Mats <willey@uw.edu>
1999-08-21 00:44:47 +0000

    a5release4
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@168 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_version.h
+++ src/pbc_version.h
@@ -1,14 +1,31 @@
+/*
 
-/* Copyright 1999, University of Washington.  All rights reserved. */
+    Copyright 1999, University of Washington.  All rights reserved.
+
+     ____        _                     _    _
+    |  _ \ _   _| |__   ___ ___   ___ | | _(_) ___
+    | |_) | | | | '_ \ / __/ _ \ / _ \| |/ / |/ _ \
+    |  __/| |_| | |_) | (_| (_) | (_) |   <| |  __/
+    |_|    \__,_|_.__/ \___\___/ \___/|_|\_\_|\___|
+
+
+    All comments and suggestions to pubcookie@cac.washington.edu
+    More info: https:/www.washington.edu/pubcookie/
+    Written by the Pubcookie Team
+
+    this is simply a header file with the version in it.  do you like that 
+       there are more lines of comment then code?
+
+ */
 
 /*
-    $Id: pbc_version.h,v 1.14 1999-08-11 23:01:00 willey Exp $
+    $Id: pbc_version.h,v 1.15 1999-08-21 00:44:47 willey Exp $
  */
 
 #ifndef PUBCOOKIE_VERSION
 #define PUBCOOKIE_VERSION
 
 #define PBC_VERSION "a5"
-#define PBC_TESTID "release3"
+#define PBC_TESTID "release4"
 
 #endif /* !PUBCOOKIE_VERSION */
