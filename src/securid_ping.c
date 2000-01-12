884e7bc4f66f3ad6ee13fafa6654cf15ab41f6cf
Steve Mats Mats <willey@uw.edu>
2000-01-12 03:39:43 +0000

    wildly reoganized version
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@191 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/securid_ping.c
+++ src/securid_ping.c
@@ -7,6 +7,11 @@
 **  Ping: Use ICMP "echo" to figure out which host interface is closest.
 */
 
+/* added by steve 11/19/99 for linux5 */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -52,6 +57,147 @@ ping_alarm(int sig)
 
   terminate = 1;
 
+}
+
+/*
+**  Checksum routine for Internet Protocol family headers (C Version)
+*/
+
+static u_short ping_cksum(addr, len)
+u_short *addr;
+int len;
+{
+  register int nleft = len;
+  register u_short *w = addr;
+  register u_short answer;
+  register int sum = 0;
+  u_short odd_byte = 0;
+
+  /*
+   *  Our algorithm is simple, using a 32 bit accumulator (sum),
+   *  we add sequential 16 bit words to it, and at the end, fold
+   *  back all the carry bits from the top 16 bits into the lower
+   *  16 bits.
+   */
+  while( nleft > 1 )  {
+    sum += *w++;
+    nleft -= 2;
+  }
+
+  /* mop up an odd byte, if necessary */
+  if( nleft == 1 ) {
+    *(u_char *)(&odd_byte) = *(u_char *)w;
+    sum += odd_byte;
+  }
+
+  /*
+   * add back carry outs from top 16 bits to low 16 bits
+   */
+  sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
+  sum += (sum >> 16);			/* add carry */
+  answer = ~sum;				/* truncate to 16 bits */
+  return (answer);
+}
+
+/*
+** 			P I N G E R
+** 
+**  Compose and transmit an ICMP ECHO REQUEST packet.  The IP packet
+**  will be added on by the kernel.  The ID field is our UNIX process ID,
+**  and the sequence number is an ascending integer.  The first 8 bytes
+**  of the data portion are used to hold a UNIX "timeval" struct in VAX
+**  byte-order, to compute the round-trip time.
+*/
+
+#define MAXHOST 10
+
+static
+void child(char *base)
+{
+  static u_char outpack[MAXPACKET];
+  register struct icmp *icp = (struct icmp *) outpack;
+  int i, cc, r, n;
+  int datalen;		/* How much data */
+/* unused ??? */
+/*  register struct timeval *tp = (struct timeval *) &outpack[8]; */
+  register u_char *datap = &outpack[8+sizeof(struct timeval)];
+  char server[128];
+  int  nhost, shost;
+  struct sockaddr_in addr[MAXHOST];
+  pid_t ppid;
+
+  ppid = getppid();
+
+  icp->icmp_type  = ICMP_ECHO;
+  icp->icmp_code  = 0;
+  icp->icmp_cksum = 0;
+  icp->icmp_seq   = 20;
+  icp->icmp_id    = ident;		/* ID */
+
+  datalen = 64-8;
+  cc = datalen+8;			/* skips ICMP portion */
+
+  for (i=8; i<datalen; i++)		/* skip 8 for time */
+    *datap++ = i;
+
+  /* Compute ICMP checksum here */
+  icp->icmp_cksum = ping_cksum( icp, cc );
+
+  nhost = MAXHOST;
+  shost = -1;
+  terminate = 0;
+
+  while (!terminate) {
+    int bad = 0;
+    for (r = i = 0; i < nhost; i++) {
+      if (i > shost) {
+        struct hostent *hp;
+        sprintf(server,"%s%d.u.washington.edu",base,++r);
+        if ( (hp = gethostbyname(server)) ) {
+	  char *cp;
+          addr[i].sin_family = hp->h_addrtype;
+          bcopy(hp->h_addr, (caddr_t)&addr[i].sin_addr, hp->h_length);
+          shost = i;
+
+	  /*
+	  **  If we've got room, check for a back door interface that
+	  **  we can reach.
+	  */
+
+	  if (i+1 < nhost && (cp = strchr(hp->h_name,'.'))) {
+	    *cp++ = '\0';
+	    sprintf(server,"%sb.%s",hp->h_name,cp);
+	    if ( (hp = gethostbyname(server)) ) {
+	      addr[i+1].sin_family = hp->h_addrtype;
+              bcopy(hp->h_addr, (caddr_t)&addr[i+1].sin_addr, hp->h_length);
+	      shost = i+1;
+	    }
+	  }
+        } else if (i == 0) {
+          fprintf(stderr,"Unknown host: %s",server);
+	  kill(ppid,SIGALRM);
+          exit(1);
+        } else {
+          nhost = i;
+          break;
+        }
+      }
+      n = sendto(s, outpack, cc, 0, SA&addr[i], sizeof(struct sockaddr));
+
+      if (n != cc)  {
+        if (n < 0 && errno != ENETUNREACH && errno != EHOSTUNREACH) {
+	  perror(base);
+	}
+        bad++;
+        if (bad >= nhost) {
+          fprintf(stderr,"Unable to ping %s server.\n",base);
+	  kill(ppid,SIGALRM);
+          exit(1);
+        }
+      }
+    }
+    sleep(1);
+  }
 }
 
 int
@@ -139,145 +285,4 @@ ping(char *base, struct sockaddr_in *sin_tcp)
     sin_tcp->sin_addr   = sin_icmp.sin_addr;
     return 0;
   }
-}
-
-/*
-** 			P I N G E R
-** 
-**  Compose and transmit an ICMP ECHO REQUEST packet.  The IP packet
-**  will be added on by the kernel.  The ID field is our UNIX process ID,
-**  and the sequence number is an ascending integer.  The first 8 bytes
-**  of the data portion are used to hold a UNIX "timeval" struct in VAX
-**  byte-order, to compute the round-trip time.
-*/
-
-#define MAXHOST 10
-
-static
-child(char *base)
-{
-  static u_char outpack[MAXPACKET];
-  register struct icmp *icp = (struct icmp *) outpack;
-  int i, cc, r, n;
-  int datalen;		/* How much data */
-  register struct timeval *tp = (struct timeval *) &outpack[8];
-  register u_char *datap = &outpack[8+sizeof(struct timeval)];
-  char server[128];
-  int  nhost, shost;
-  struct sockaddr_in addr[MAXHOST];
-  pid_t ppid;
-
-  ppid = getppid();
-
-  icp->icmp_type  = ICMP_ECHO;
-  icp->icmp_code  = 0;
-  icp->icmp_cksum = 0;
-  icp->icmp_seq   = 20;
-  icp->icmp_id    = ident;		/* ID */
-
-  datalen = 64-8;
-  cc = datalen+8;			/* skips ICMP portion */
-
-  for (i=8; i<datalen; i++)		/* skip 8 for time */
-    *datap++ = i;
-
-  /* Compute ICMP checksum here */
-  icp->icmp_cksum = ping_cksum( icp, cc );
-
-  nhost = MAXHOST;
-  shost = -1;
-  terminate = 0;
-
-  while (!terminate) {
-    int bad = 0;
-    for (r = i = 0; i < nhost; i++) {
-      if (i > shost) {
-        struct hostent *hp;
-        sprintf(server,"%s%d.u.washington.edu",base,++r);
-        if (hp = gethostbyname(server)) {
-	  char *cp;
-          addr[i].sin_family = hp->h_addrtype;
-          bcopy(hp->h_addr, (caddr_t)&addr[i].sin_addr, hp->h_length);
-          shost = i;
-
-	  /*
-	  **  If we've got room, check for a back door interface that
-	  **  we can reach.
-	  */
-
-	  if (i+1 < nhost && (cp = strchr(hp->h_name,'.'))) {
-	    *cp++ = '\0';
-	    sprintf(server,"%sb.%s",hp->h_name,cp);
-	    if (hp = gethostbyname(server)) {
-	      addr[i+1].sin_family = hp->h_addrtype;
-              bcopy(hp->h_addr, (caddr_t)&addr[i+1].sin_addr, hp->h_length);
-	      shost = i+1;
-	    }
-	  }
-        } else if (i == 0) {
-          fprintf(stderr,"Unknown host: %s",server);
-	  kill(ppid,SIGALRM);
-          exit(1);
-        } else {
-          nhost = i;
-          break;
-        }
-      }
-      n = sendto(s, outpack, cc, 0, SA&addr[i], sizeof(struct sockaddr));
-
-      if (n != cc)  {
-        if (n < 0 && errno != ENETUNREACH && errno != EHOSTUNREACH) {
-	  perror(base);
-	}
-        bad++;
-        if (bad >= nhost) {
-          fprintf(stderr,"Unable to ping %s server.\n",base);
-	  kill(ppid,SIGALRM);
-          exit(1);
-        }
-      }
-    }
-    sleep(1);
-  }
-}
-
-/*
-**  Checksum routine for Internet Protocol family headers (C Version)
-*/
-
-static
-ping_cksum(addr, len)
-u_short *addr;
-int len;
-{
-  register int nleft = len;
-  register u_short *w = addr;
-  register u_short answer;
-  register int sum = 0;
-  u_short odd_byte = 0;
-
-  /*
-   *  Our algorithm is simple, using a 32 bit accumulator (sum),
-   *  we add sequential 16 bit words to it, and at the end, fold
-   *  back all the carry bits from the top 16 bits into the lower
-   *  16 bits.
-   */
-  while( nleft > 1 )  {
-    sum += *w++;
-    nleft -= 2;
-  }
-
-  /* mop up an odd byte, if necessary */
-  if( nleft == 1 ) {
-    *(u_char *)(&odd_byte) = *(u_char *)w;
-    sum += odd_byte;
-  }
-
-  /*
-   * add back carry outs from top 16 bits to low 16 bits
-   */
-  sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
-  sum += (sum >> 16);			/* add carry */
-  answer = ~sum;				/* truncate to 16 bits */
-  return (answer);
 }
