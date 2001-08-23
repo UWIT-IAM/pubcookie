07fb38b54aa3a49ad152a4272ce2aaedd9801392
Steve Mats Mats <willey@uw.edu>
2001-08-23 17:19:43 +0000

    now uses the real auth_kdc module mojor revamp
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@341 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/uwnetid_stub.c
+++ src/uwnetid_stub.c
@@ -19,7 +19,7 @@
  */
 
 /*
-    $Id: uwnetid_stub.c,v 1.2 2001-05-29 20:49:48 willey Exp $
+    $Id: uwnetid_stub.c,v 1.3 2001-08-23 17:19:43 willey Exp $
  */
 
 
@@ -83,7 +83,7 @@ void send_pilot_message(char *message)
 }
 
 /* logs the message and forwards it on to pilot                               */
-void log_error(const char *format,...)
+void log_error(int grade, const char *service, int self_clearing, const char *format,...)
 {
     va_list	args;
     char	new_format[PBC_4K];
@@ -103,7 +103,7 @@ void log_error(const char *format,...)
 void abend(char *message) 
 {
 
-    log_error(message);
+    log_error(0, "test", 0, message);
     exit(0);
 
 }
@@ -116,121 +116,15 @@ char *check_login_uwnetid(char *user, char *pass)
     fprintf(stderr, "check_login_uwnetid: hello\n");
 #endif 
 
-    if( auth_kdc(user, pass) == OK )
+    if( auth_kdc(user, pass) == NULL )
         return(CHECK_LOGIN_RET_SUCCESS);
     else
         return(CHECK_LOGIN_RET_FAIL);
 
 }
 
-
-int auth_kdc(char *username, char *passwd)
+void clear_error(const char *service, const char *message)
 {
-
-#define KRB5_DEFAULT_OPTIONS 0
-#define KRB5_DEFAULT_LIFE 60*60*10 /* 10 hours */
-
-extern int optind;
-extern char *optarg;
-
-krb5_data tgtname = {
-    0,
-    KRB5_TGS_NAME_SIZE,
-    KRB5_TGS_NAME
-};
-
-/*
- * Try no preauthentication first; then try the encrypted timestamp
- */
-
-krb5_preauthtype * preauth = NULL;
-krb5_context kcontext;
-krb5_deltat lifetime = KRB5_DEFAULT_LIFE;       /* -l option */
-int options = KRB5_DEFAULT_OPTIONS;
-krb5_error_code code;
-krb5_principal me;
-krb5_principal kserver;
-krb5_creds my_creds;
-krb5_timestamp now;
-krb5_address **addrs = (krb5_address **)0;
-char *client_name;
-
-    int		ret = 1;
-
-    code = krb5_init_context(&kcontext);
-    if(code) {
-        log_error("auth_kdc: %s while initializing krb5\n", 
-			error_message(code));
-	abend("can't init krb5 context");
-    }
-
-    if((code = krb5_timeofday(kcontext, &now))) {
-	log_error("auth_kdc: %s while getting time of day\n", 
-			error_message(code));
-	abend("can't get the time of day");
-    }
-
-    /* just use the name we give you and default domain */
-    if ((code = krb5_parse_name (kcontext, username, &me))) {
-	 log_error("auth_kdc: ABEND %s when parsing name %s\n", 
-			error_message(code), username);
-	 abend("krb5 can't parse username");
-    }
-    
-    if ((code = krb5_unparse_name(kcontext, me, &client_name))) {
-	log_error("auth_kdc: %s when unparsing name\n", 
-			error_message(code));
-	abend("misc. krb5 problem");
-    }
-
-    memset((char *)&my_creds, 0, sizeof(my_creds));
-    
-    /* me is the pricipal */
-    my_creds.client = me;
-
-    /* get kserver name */
-    if((code = krb5_build_principal_ext(kcontext, &kserver,
-                        krb5_princ_realm(kcontext, me)->length,
-                        krb5_princ_realm(kcontext, me)->data,
-                        tgtname.length, tgtname.data,
-                        krb5_princ_realm(kcontext, me)->length,
-                        krb5_princ_realm(kcontext, me)->data,
-                        0))) {
-	log_error("auth_kdc: %s while building kserver name\n", 
-			error_message(code));
-	return(FAIL);
-    }
-	
-    my_creds.server = kserver;
-
-    my_creds.times.starttime = 0;	/* start timer when request
-					   gets to KDC */
-    my_creds.times.endtime = now + lifetime;
-
-    my_creds.times.renew_till = 0;
-
-    code = krb5_get_in_tkt_with_password(kcontext, options, addrs,
-					      NULL, preauth, passwd, 0,
-					      &my_creds, 0);
-
-    memset(passwd, 0, sizeof(passwd));
-    
-    if (code) {
-	if (code == KRB5KRB_AP_ERR_BAD_INTEGRITY)
-	    log_message("auth_kdc: Password incorrect username: %s\n", 
-			username);
-	else 
-	    log_message("auth_kdc: %s while checking credntials username: %s\n",
-			error_message(code), username);
-	ret = FAIL;
-    }
-
-    /* my_creds is pointing at server */
-    krb5_free_principal(kcontext, kserver);
-
-    krb5_free_context(kcontext);
-    
-    return(ret);
 
 }
 
@@ -242,7 +136,7 @@ int argc;
 char **argv;
 {
   char   buf[1024];
-  char   name[9], prn[7], junk[20];
+  char   name[128], pass[128], junk[20];
   int    i;
 
   printf("want: name <userid> pass <password>\n");
@@ -250,14 +144,14 @@ char **argv;
   while ( fgets(buf, 1024, stdin) ) {
       sscanf (buf, "%s", junk);
       if ( ! strcmp(junk, "exit") ) break;
-      i=sscanf (buf, "name %s pass %s", name, prn);
-//      printf ("\ti ->%d<- name ->%s<- prn ->%s<-\n", i, name, prn);
+      i=sscanf (buf, "name %s pass %s", name, pass);
+      printf ("\ti ->%d<- name ->%s<- pass ->%s<-\n", i, name, pass);
 
       ( i == 2 ) ?
-          check_login_uwnetid(name,prn) == CHECK_LOGIN_RET_SUCCESS ? printf("auth ok\n") : printf("auth fail\n")
+          check_login_uwnetid(name,pass) == CHECK_LOGIN_RET_SUCCESS ? printf("auth ok\n") : printf("auth fail\n")
       :
           printf("didn't get what i wanted\n");
-      *prn='\0'; *name='\0';
+      *pass='\0'; *name='\0';
   }
 
   exit(0);
