7e9596b97692415fd127831bd2f5d06d0d8d3b3a
Steve Mats Mats <willey@uw.edu>
2001-09-28 17:27:25 +0000

    matches index.cgi.c version 1.26 bunch of function prototype changes and macro additions
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@361 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/index.cgi.h
+++ src/index.cgi.h
@@ -18,7 +18,7 @@
  */
 
 /*
-    $Id: index.cgi.h,v 1.9 2001-08-23 21:17:29 willey Exp $
+    $Id: index.cgi.h,v 1.10 2001-09-28 17:27:25 willey Exp $
  */
 
 typedef struct {
@@ -47,7 +47,9 @@ typedef struct {
     time_t	last_ts;
     int		serial;
     int		next_securid;
+    int		session_reauth;
     char	*first_kiss;
+    char	reply;
 } login_rec;
 
 /* prototypes */
@@ -66,13 +68,13 @@ void notok_no_g();
 void notok_formmultipart();
 void notok_generic();
 void notok_bad_agent();
-void print_login_page_part1(int);
+void print_login_page_part1(char *);
 void print_login_page_part5();
 int check_user_agent();
 void log_message(const char *, ...);
 void log_error(int, const char *, int, const char *, ...);
 void clear_error(const char *, const char *);
-void print_login_page(login_rec *, char *, char *, int, int);
+void print_login_page(login_rec *, login_rec *, char *, char *, int, int);
 void print_login_page_lhs1(char *, char *, char *);
 void print_login_page_lhs2(login_rec *);
 void print_login_page_centre();
@@ -81,21 +83,29 @@ void print_login_page_bottom();
 void print_uwnetid_logo();
 void print_login_page_hidden_stuff(login_rec *);
 void print_login_page_expire_info();
-login_rec *verify_login_cookie (char *, login_rec *);
+login_rec *verify_unload_login_cookie (login_rec *);
 int create_cookie(char *, char *, char *, char, char, int, char *, int);
 login_rec *get_query();
 char *check_login(login_rec *);
-char *check_l_cookie(login_rec *);
-void print_redirect_page(login_rec *);
+char *check_l_cookie(login_rec *, login_rec *);
+void print_redirect_page(login_rec *, login_rec *);
 int get_next_serial();
 char *url_encode();
 char *get_cookie_created(char *);
 char *decode_granting_request(char *);
+int ride_free_zone(login_rec *, login_rec *);
 
+#define RIDE_FREE_TIME (10 * 60)
 #define LOGIN_DIR "/"
 #define THIS_CGI "cindex.cgi"
 #define REFRESH "0"
 #define EXPIRE_LOGIN 60 * 60 * 8
+
+/* why print login page ? */
+#define LOGIN_REASON_AUTH_FAIL   "bad auth"
+#define LOGIN_REASON_SECURID     "securid requires reauth"
+#define LOGIN_REASON_NO_L        "no L cookie yet"
+#define LOGIN_REASON_SESS_REAUTH "session timeout requires reauth"
 
 /* some messages about people who hit posts and don't have js on */
 #define PBC_POST_NO_JS_TEXT "Thank you for logging in\n"
@@ -116,6 +126,9 @@ char *decode_granting_request(char *);
 #define PROMPT_PASSWD "<B>Password:</B><BR>"
 #define PROMPT_SECURID "<B>Securid:</B><BR>"
 #define PROMPT_INVALID "<B>BOGUS:</B><BR>"
+
+/* tags the request as a reply from the form */
+#define FORM_REPLY '1'
 
 /* replacement string for g req cookies once they hav gone thru the cgi */
 #define G_REQ_RECIEVED "g req received"
