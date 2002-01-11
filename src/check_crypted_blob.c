1cc6c89e28afbdf5a65b0c92872d7acd1e2edca9
Steve Mats Mats <willey@uw.edu>
2002-01-11 23:40:05 +0000

    WIN32 changes from JimB
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@506 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/check_crypted_blob.c
+++ src/check_crypted_blob.c
@@ -1,20 +1,50 @@
+/*
 
-/* Copyright 1999, University of Washington.  All rights reserved. */
+    Copyright 1999-2002, University of Washington.  All rights reserved.
 
+     ____        _                     _    _
+    |  _ \ _   _| |__   ___ ___   ___ | | _(_) ___
+    | |_) | | | | '_ \ / __/ _ \ / _ \| |/ / |/ _ \
+    |  __/| |_| | |_) | (_| (_) | (_) |   <| |  __/
+    |_|    \__,_|_.__/ \___\___/ \___/|_|\_\_|\___|
+
+
+    All comments and suggestions to pubcookie@cac.washington.edu
+    More info: http://www.washington.edu/computing/pubcookie/
+    Written by the Pubcookie Team
+
+ */
+
+/* check_crypted_blob.c  */
+
+#if !defined(WIN32)
 #include <netdb.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <pem.h>
+#if defined (WIN32) 
+#include <winsock2.h>   // jimb - WSASTARTUP for gethostname
+#include <getopt.h>     // jimb - getopt from pdtools
+extern char * optarg;
+#define bzero(s,n)	memset((s),0,(n))  // jimb - win32
+#else
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#endif
 #include "pubcookie.h"
 #include "libpubcookie.h"
 #include "pbc_config.h"
 #include "pbc_version.h"
+
+#if defined (WIN32)
+extern int Debug_Trace = 0;
+extern FILE *debugFile = NULL;
+#endif
 
 void usage(const char *progname) {
     printf("%s -c crypted_file [-k c_key_file] [-h]\n\n", progname);
@@ -32,6 +62,14 @@ int main(int argc, char **argv) {
     int 		c, barfarg = 0;
     char		*key_file = NULL;
     char		*crypted_file = NULL;
+#if defined (WIN32)
+    char		SystemRoot[256];
+
+    Debug_Trace = 1;
+    debugFile = stdout;
+#endif
+	
+    printf("check_crypted_blob\n\n");
 
     bzero(in, 1024);
     bzero(out, 1024);
@@ -47,7 +85,7 @@ int main(int argc, char **argv) {
 	case 'c' :
             if( crypted_file != NULL ) {
 	        usage(argv[0]);
-	        exit;
+		break;
 	    }
 	    crypted_file = strdup(optarg);
 	    break;
@@ -57,42 +95,71 @@ int main(int argc, char **argv) {
 	default :
             if( crypted_file != NULL ) {
 	        usage(argv[0]);
-	        exit;
+		break;
 	    }
 	    crypted_file = strdup(optarg);
 	    break;
 	}
     }
 
+#if defined(WIN32)                                           
+    {   
+	WSADATA wsaData;
+
+	if( WSAStartup((WORD)0x0101, &wsaData ) ) 
+	{  
+	    printf( "Unable to initialize WINSOCK: %d", WSAGetLastError() );
+	    return -1;
+	}
+    }   
+#endif
+
     if ( key_file )
         c1_stuff = libpbc_init_crypt(key_file);
-    else
-        c1_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
+    else {
+	key_file = malloc(256);
+#if defined(WIN32)  
+	GetEnvironmentVariable ("windir",SystemRoot,256);
+        sprintf(key_file,"%s%s", SystemRoot,PBC_CRYPT_KEYFILE);
+#else
+        sprintf(key_file,"%s",PBC_CRYPT_KEYFILE);
+#endif
+	printf("Using c_key file: %s\n\n",key_file);
+        c1_stuff = libpbc_init_crypt(key_file);
+    }
 
     if ( c1_stuff == NULL ) {
 	printf("unable to initialize encryption context\n");
         usage(argv[0]);
-	exit;
     }
 
     if ( crypted_file != NULL ) {
-        if( ! (cfp = pbc_fopen(crypted_file, "r")) )
-            libpbc_abend("cannot open the crypted file %s\n", crypted_file);
+        if( ! (cfp = pbc_fopen(crypted_file, "r")) ) {
+            libpbc_abend("\n*** Cannot open the crypted file %s\n", crypted_file);
+            exit(1);
+	}
         fread(intermediate, sizeof(char), PBC_1K, cfp);
     } else {
-	printf("Must specify file with ciphertext\n\n");
+	printf("Must specify a file with ciphertext\n\n");
 	usage(argv[0]);
-	exit;
     }
 
-    if ( ! libpbc_decrypt_cookie(intermediate, out, c1_stuff, strlen(in)) )
-        exit(0);
-    printf("encrypted message is: %s\n", out);
+    if ( ! libpbc_decrypt_cookie(intermediate, out, c1_stuff, strlen(in)) ) {
+	printf("\n*** Libpbc_decrypt_cookie failed\n");
+        exit(1);
+    }
+
+    printf("\nencrypted message is: %s\n", out);
 
     if( memcmp(in,out,sizeof(in)) != 0 )
-	printf("cfb64 encrypt/decrypt error.\n");
+	printf("\n*** cfb64 encrypt/decrypt error ***!\n");
     else
-	printf("Yeah!  It worked\n");
-    exit(1);
+	printf("\nYeah!  It worked\n\n");
+
+#if defined(WIN32)  
+    WSACleanup();
+#endif
+
+    exit(0);
 
 }
--- src/make_crypted_blob.c
+++ src/make_crypted_blob.c
@@ -1,7 +1,6 @@
-
 /*
 
-    Copyright 1999-2001, University of Washington.  All rights reserved.
+    Copyright 1999-2002, University of Washington.  All rights reserved.
 
      ____        _                     _    _
     |  _ \ _   _| |__   ___ ___   ___ | | _(_) ___
@@ -17,18 +16,38 @@
  */
 
 /*
-    $Id: make_crypted_blob.c,v 1.2 2002-01-10 23:16:32 willey Exp $
+    $Id: make_crypted_blob.c,v 1.3 2002-01-11 23:40:05 willey Exp $
  */
 
 
-#include <string.h>
-#include <time.h>
+#if !defined(WIN32)
+#include <netdb.h>
+#endif
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include <pem.h>
+#if defined (WIN32) 
+#include <winsock2.h>   // jimb - WSASTARTUP for gethostname
+#include <getopt.h>     // jimb - getopt from pdtools
+extern char * optarg;
+#define bzero(s,n)	memset((s),0,(n))  // jimb - win32
+#else
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
 #include "pubcookie.h"
 #include "libpubcookie.h"
 #include "pbc_config.h"
+#include "pbc_version.h"
+
+#if defined (WIN32)
+extern int Debug_Trace = 0;
+extern FILE *debugFile = NULL;
+#endif
 
 void usage(const char *progname) {
     printf("%s [-o out_file] [-k c_key_file] [-h]\n\n", progname);
@@ -45,6 +64,14 @@ int main(int argc, char **argv) {
     char		*out_file = NULL;
     FILE		*ofp;
     int 		c, barfarg = 0;
+#if defined (WIN32)
+    char		SystemRoot[256];
+
+    Debug_Trace = 1;
+    debugFile = stderr;
+#endif
+
+    fprintf(stderr,"make_crypted_blob\n\n");
 
     optarg = NULL;
     while (!barfarg && ((c = getopt(argc, argv, "ho:k:")) != -1)) {
@@ -64,29 +91,59 @@ int main(int argc, char **argv) {
 	}
     }
 
+#if defined(WIN32)                                           
+	{   
+	WSADATA wsaData;
+
+	if( WSAStartup((WORD)0x0101, &wsaData ) ) 
+	{  
+	    fprintf(stderr,"Unable to initialize WINSOCK: %d", WSAGetLastError() );
+	    return -1;
+	}
+	}   
+#endif
+
     if ( key_file )
         c1_stuff = libpbc_init_crypt(key_file);
-    else
-        c1_stuff = libpbc_init_crypt(PBC_CRYPT_KEYFILE);
+    else {
+	key_file = malloc(256);
+#if defined(WIN32)  
+	GetEnvironmentVariable ("windir",SystemRoot,256);
+        sprintf(key_file,"%s%s", SystemRoot,PBC_CRYPT_KEYFILE);
+#else
+        sprintf(key_file,"%s",PBC_CRYPT_KEYFILE);
+#endif
+	fprintf(stderr,"Using c_key file: %s\n\n",key_file);
+        c1_stuff = libpbc_init_crypt(key_file);
+	}
 
     bzero(in, 1024);
     bzero(intermediate, 1024);
     strcpy(in, "Maybe this plaintext is another world's ciphertext.");
 
-    fprintf(stderr, "ciphertext sent to stdout\n");
-    if ( ! libpbc_encrypt_cookie(in, intermediate, c1_stuff, strlen(in)) )
-        exit(0);
+    
+    if ( ! libpbc_encrypt_cookie(in, intermediate, c1_stuff, strlen(in)) ) {
+        printf("\n*** Libpbc_encrypt_cookie failed\n");
+        exit(1);
+    }
 
     if ( out_file ) {
-        if( ! (ofp = pbc_fopen(out_file, "w")) )
-            libpbc_abend("cannot open the out file %s\n", out_file);
+	fprintf(stderr, "\nciphertext sent to %s\n\n",out_file);
+	if( ! (ofp = pbc_fopen(out_file, "w")) ) {
+            libpbc_abend("\n*** Cannot open the out file %s\n", out_file);
+	    exit(1);
+	}
     } else {
+	fprintf(stderr, "\nciphertext sent to stdout\n\n");
 	ofp = stdout;
     }
 
     fprintf(ofp, "%s", intermediate); 
 
-    exit(1);
+#if defined(WIN32)  
+    WSACleanup();
+#endif
+
+    exit(0);
 
 }
-
