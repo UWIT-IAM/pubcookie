b31719b1fbb7fb82e88fe364409d713169f47aed
Steve Mats Mats <willey@uw.edu>
2002-05-15 20:25:25 +0000

    short-term logout
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@577 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/index.cgi.h
+++ src/index.cgi.h
@@ -18,7 +18,7 @@
  */
 
 /*
-    $Id: index.cgi.h,v 1.19 2002-03-26 01:14:21 willey Exp $
+    $Id: index.cgi.h,v 1.20 2002-05-15 20:25:25 willey Exp $
  */
 
 #ifndef PUBCOOKIE_LOGIN_CGI
@@ -48,14 +48,15 @@ typedef struct {
     char	*referer;
     char	type;
     time_t	create_ts;
-    time_t	last_ts;
+    time_t	expire_ts;
     int		serial;
     int		next_securid;
     int		session_reauth;
     int		duration;
     char	*first_kiss;
-    char	reply;
+    int		reply;
     int		alterable_username;
+    int		pinit;
 } login_rec;
 
 struct browser {
@@ -163,7 +164,7 @@ void print_uwnetid_logo();
 void print_login_page_hidden_stuff(login_rec *);
 void print_login_page_expire_info();
 login_rec *verify_unload_login_cookie (login_rec *);
-int create_cookie(char *, char *, char *, char, char, int, char *, char *, int);
+int create_cookie(char *, char *, char *, char, char, int, time_t, char *, char *, int);
 login_rec *get_query();
 char *check_login(login_rec *, login_rec *);
 char *check_l_cookie(login_rec *, login_rec *);
@@ -173,13 +174,20 @@ char *url_encode();
 char *get_cookie_created(char *);
 char *decode_granting_request(char *);
 char ride_free_zone(login_rec *, login_rec *);
-const char *get_domain_hostname();
+const char *login_host();
+const char *enterprise_domain();
+char *login_private_keyfile();
+int init_crypt();
+int set_pinit_cookie();
+int clear_pinit_cookie();
 
 #define RIDE_FREE_TIME (10 * 60)
 #define LOGIN_DIR "/"
 #define THIS_CGI "cindex.cgi"
 #define REFRESH "0"
-#define EXPIRE_LOGIN 60 * 60 * 8
+#define DEFAULT_LOGIN_EXPIRE (8 * 60 * 60)
+#define APP_LOGOUT_STR "app_logout_string"
+#define APP_LOGOUT_STR_SEP '-'
 
 #define TMPL_FNAME "/usr/local/pubcookie/login_templates/"
 
@@ -189,10 +197,15 @@ const char *get_domain_hostname();
 #define LOGIN_REASON_NO_L        "no L cookie yet"
 #define LOGIN_REASON_SESS_REAUTH "session timeout requires reauth"
 
+/* the pinit cookie is used to transition from a pinit login to 
+   a pinit responce */
+#define PBC_PINIT_COOKIENAME "pinit"
+
 /* some messages about people who hit posts and don't have js on */
 #define PBC_POST_NO_JS_TEXT "Thank you for logging in\n"
 
 #define PRINT_LOGIN_PLEASE "Please log in."
+#define PRINT_LOGIN_PINIT "Welcome to the UW \"weblogin\" service. Please log in to identify yourself."
 #define TROUBLE_CREATING_COOKIE "Trouble creating cookie.  Please re-enter."
 #define PROBLEMS_PERSIST "If problems persist contact help@cac.washington.edu."
 #define AUTH_FAILED_MESSAGE1 "Login failed.  Please re-enter.\n"
@@ -203,16 +216,20 @@ const char *get_domain_hostname();
 #define CHECK_LOGIN_RET_SUCCESS "success"
 #define CHECK_LOGIN_RET_FAIL "fail"
 
+/* special strings about time remaining */
+#define REMAINING_EXPIRED "expired"
+#define REMAINING_UNKNOWN "unknown"
+
 #define PROMPT_UWNETID "<B>UW NetID:</B><BR>"
 #define PROMPT_PASSWD "<B>Password:</B><BR>"
 #define PROMPT_SECURID "<B>Securid:</B><BR>"
 #define PROMPT_INVALID "<B>BOGUS:</B><BR>"
 
 /* tags the request as a reply from the form */
-#define FORM_REPLY '1'
+#define FORM_REPLY 1
 
 /* replacement string for g req cookies once they hav gone thru the cgi */
-#define G_REQ_RECIEVED "g req received"
+#define G_REQ_RECEIVED "g req received"
 
 /* how we accentuate warning messages */
 #define PBC_EM1_START "<P><B><FONT COLOR=\"#FF0000\" SIZE=\"+1\">" 
