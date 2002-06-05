f1d4a4e1dc83365c36177804e7783be1d6b20b6c
greenfld <greenfld@uw.edu>
2002-06-05 17:18:14 +0000

    remove unused kerberos foo
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@613 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/contrib/cmu_kerberos/k4auth.h
+++
@@ -1,4 +0,0 @@
-const char *kerberos4_verify_password(const char *user,
-				      const char *passwd,
-				      const char *principal,
-				      char *path_to_srvtab);
--- src/contrib/cmu_kerberos/k5auth.h
+++
@@ -1,4 +0,0 @@
-const char *kerberos5_verify_password(const char *user,
-				      const char *passwd,
-				      const char *principal,
-				      char *path_to_srvtab);
