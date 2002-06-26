5c7fcd864898885ceb4c99ef893ac6bd1268af90
greenfld <greenfld@uw.edu>
2002-06-26 20:39:19 +0000

    make it compile
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@697 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_logging.c
+++ src/pbc_logging.c
@@ -10,7 +10,7 @@ void pbc_log_init()
 /* open syslog - we are appending the PID to the log, and prepending 
     the string "pubcookie login server" to make it easily greppable 
 */
-  openlog("pubcookie login server", LOG_PID);
+    openlog("pubcookie login server", LOG_PID, LOG_LOCAL2);
 }
 
 
@@ -20,14 +20,16 @@ void pbc_log_activity(int logging_level, const char *message,...)
   char      new_message[PBC_4K];
   char      log[PBC_4K];
 
-  if (logging_level <= (libpbc_config_getint("logging_level", logging_level)))
-    {
+  if (logging_level <= (libpbc_config_getint("logging_level", logging_level)))    {
       va_start(args, message);
+#if 0
       /* should I move the timestamp here? */
       snprintf(new_message, sizeof(new_message)-1, "%s: %s",
 	       SYSERR_LOGINSRV_MESSAGE, message);
       vsnprintf(log, sizeof(log)-1, new_message, args);
-
+#else
+      vsnprintf(log, sizeof(log)-1, message, args);
+#endif
       /* write to syslog, making it a "private auth"  message.
      This seemed to be the most reasonable option...I originally had
      it a configurable option, but decided against it. */
@@ -46,9 +48,11 @@ void pbc_log_close()
   closelog();
 }
 
+#if 0
 char* pbc_create_log_message(char *info, char* user, char* app_id)
 {
   return sprintf(%s: user ip: %s \t app id: %s \n %s, 
 libpbc_time_string(time(NULL)),user,app_id, info);
   
 }
+#endif
