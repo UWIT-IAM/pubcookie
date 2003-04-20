662ddd723a0954ff4102e4cc80c188ce64b9fbf2
jjminer <jjminer@uw.edu>
2003-04-20 16:07:20 +0000

    Updated version numbers.
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1080 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_version.h
+++ src/pbc_version.h
@@ -19,7 +19,7 @@
  */
 
 /*
-    $Id: pbc_version.h,v 1.51 2003-04-19 18:59:40 jjminer Exp $
+    $Id: pbc_version.h,v 1.52 2003-04-20 16:07:20 jjminer Exp $
  */
 
 #ifndef PUBCOOKIE_VERSION
@@ -46,6 +46,6 @@
  * configure should probably set this.
  */
 
-#define PBC_VERSION_STRING "3.0.0 pre-beta4"
+#define PBC_VERSION_STRING "3.0.0 rc1"
 
 #endif /* !PUBCOOKIE_VERSION */
