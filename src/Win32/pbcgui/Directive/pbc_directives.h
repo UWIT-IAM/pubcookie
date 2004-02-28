530cbafefc2366a7c40405ec2284ae7368d51a56
Nathan Dors <dors@uw.edu>
2004-02-28 00:51:56 +0000

    edited info texts on pbc gui properties sheets
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1263 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/Win32/pbcgui/Directive/pbc_directives.h
+++ src/Win32/pbcgui/Directive/pbc_directives.h
@@ -5,25 +5,25 @@
 	directive[0].name         = L"Inactive_Timeout";
 	directive[0].type         = D_FREE_INT;
 	directive[0].value        = _itow(PBC_DEFAULT_INACT_EXPIRE,dbuffer,10);
-	directive[0].description  = L"Session cookie times out after this number of seconds without session activity";
+	directive[0].description  = L"Session cookie times out after this number of seconds without session activity.";
 	directive[0].defined_in   = L"(Program Default)";
 
 	directive[1].name         = L"Hard_Timeout";
 	directive[1].type         = D_FREE_INT;
 	directive[1].value        = _itow(PBC_DEFAULT_HARD_EXPIRE,dbuffer,10);
-	directive[1].description  = L"Session cookie times out after this value in seconds regardless of session activity";
+	directive[1].description  = L"Session cookie times out after this value in seconds regardless of session activity.";
 	directive[1].defined_in   = L"(Program Default)";
 
 	directive[2].name         = L"Session_Reauth";
 	directive[2].type         = D_FREE_INT;
 	directive[2].value        = L"0";
-	directive[2].description  = L"Require re-authentication for this resource even with valid login cookie.\n0: false\n1: true\n2+: number of seconds grace period before re-authentication is required";
+	directive[2].description  = L"Require re-authentication for this resource even with valid login cookie.\n  0 : false\n  1 : true\n  2+ : number of seconds grace period before re-authentication is required";
 	directive[2].defined_in   = L"(Program Default)";
 
 	directive[3].name         = L"AuthType";
 	directive[3].type         = D_BOUND_STRING;
 	directive[3].value        = PBC_AUTHTYPE0;
-	directive[3].description  = L"Authentication type, as offered by your login server.  The string is case-insensitive.\nThese strings can be redefined at the server or web instance level.";
+	directive[3].description  = L"Authentication type, as offered by your login server. The string is case-insensitive.\nThese strings can be redefined at the server or web instance level.";
 	directive[3].bound_val[0] = PBC_AUTHTYPE0;
 	directive[3].bound_val[1] = PBC_AUTHTYPE1;
 	directive[3].bound_val[2] = PBC_AUTHTYPE3;
@@ -32,7 +32,7 @@
 	directive[4].name         = L"Logout_Action";
 	directive[4].type         = D_BOUND_INT;
 	directive[4].value        = L"0";
-	directive[4].description  = L"Login interaction on this node:\n  0: No logout action\n  1: Clear session cookie and serve page; implicitly sets AuthType to None\n  2: Clear session cookie and redirect to login server\n  3: Clear session cookie, redirect to to login server, and clear login cookie";
+	directive[4].description  = L"Logout action on this node:\n  0 : No logout action\n  1 : Clear session cookie and serve page; implicitly sets AuthType to None\n  2 : Clear session cookie and redirect to login server\n  3 : Clear session cookie and redirect to login server to clear login cookie";
 	directive[4].bound_val[0] = L"0";
 	directive[4].bound_val[1] = L"1";
 	directive[4].bound_val[2] = L"2";
@@ -42,30 +42,30 @@
 	directive[5].name         = L"Login_URI";
 	directive[5].type         = D_FREE_STRING;
 	directive[5].value        = PBC_LOGIN_URI;
-	directive[5].description  = L"URL of Pubcookie login server";
+	directive[5].description  = L"URL of Pubcookie login server.";
 	directive[5].defined_in   = defined_in;
 
 	directive[6].name         = L"Web_Login";
 	directive[6].type         = D_FREE_STRING;
-	directive[6].description  = L"\t*Deprecated*\n\nURL of Pubcookie login server. Use Login_URI instead.  Login_URI takes precedence if both are set.";
+	directive[6].description  = L"*Deprecated*\n\nUse Login_URI instead. Login_URI takes precedence if both are set.";
 	directive[6].value        = PBC_LOGIN_URI;
 	directive[6].defined_in   = defined_in;
 
 	directive[7].name         = L"Enterprise_Domain";
 	directive[7].type         = D_FREE_STRING;
 	directive[7].value        = PBC_ENTRPRS_DOMAIN;
-	directive[7].description  = L"Domain for scoping granting request cookie";
+	directive[7].description  = L"Domain for scoping granting request cookie.";
 	directive[7].defined_in   = defined_in;
 
 	directive[8].name         = L"Error_Page";
 	directive[8].type         = D_FREE_STRING;
-	directive[8].description  = L"Partial URL path for errors than halt the pubcookie process";
+	directive[8].description  = L"Partial URL path for errors than halt the Pubcookie process.";
 	directive[8].defined_in   = L"(Program Default)";
 
 	directive[9].name         = L"SetHeaderValues";
 	directive[9].type         = D_BOUND_INT;
 	directive[9].value        = L"0";
-	directive[9].description  = L"Set Pubcookie header values even if not using Pubcookie authentication\n0:Off, Non-zero:On";
+	directive[9].description  = L"Set to 1 to enable Pubcookie header values even if not using Pubcookie authentication.";
 	directive[9].bound_val[0] = L"0";
 	directive[9].bound_val[1] = L"1";
 	directive[9].defined_in   = L"(Program Default)";
--- src/Win32/pbcgui/Server/pbc_directives.h
+++ src/Win32/pbcgui/Server/pbc_directives.h
@@ -6,7 +6,7 @@
 	directive[0].type         = D_BOUND_INT;
 	directive[0].value        = _itow(PBC_DEBUG_TRACE,dbuffer,10);
 	directive[0].defined_in   = defined_in;
-	directive[0].description  = L"Debug level:\n-1: Errors Only, 0:Warnings and Errors, 1:Informational, 2+:More Debugging";
+	directive[0].description  = L"Debug level:\n-1 : Errors Only\n 0 : Warnings and Errors\n 1 : Informational\n 2+ : More Debugging";
 	directive[0].bound_val[0] = L"-1";
 	directive[0].bound_val[1] = L"0";
 	directive[0].bound_val[2] = L"1";
@@ -17,89 +17,91 @@
 	directive[1].type         = D_FREE_STRING;
 	directive[1].value        = PBC_LOGIN_URI;
 	directive[1].defined_in   = defined_in;
-	directive[1].description  = L"URL of Pubcookie login server";
+	directive[1].description  = L"URL of Pubcookie login server.";
 
 	directive[2].name         = L"Keymgt_URI";
 	directive[2].type         = D_FREE_STRING;
 	directive[2].value        = PBC_KEYMGT_URI;
 	directive[2].defined_in   = defined_in;
-	directive[2].description  = L"URL of Pubcookie key manangement server";
+	directive[2].description  = L"URL of Pubcookie keyserver.";
 
 	directive[3].name         = L"AuthTypeName0";
 	directive[3].type         = D_FREE_STRING;
 	directive[3].value        = PBC_AUTHTYPE0;
 	directive[3].defined_in   = defined_in;
-	directive[3].description  = L"Name of authentication type that corresponds to no authentication";
+	directive[3].description  = L"Name of authentication type that corresponds to no authentication.";
 
 	directive[4].name         = L"AuthTypeName1";
 	directive[4].type         = D_FREE_STRING;
 	directive[4].value        = PBC_AUTHTYPE1;
 	directive[4].defined_in   = defined_in;
-	directive[4].description  = L"Name of authentication type that corresponds to regular pubcookie authentication, flavor_basic in a 3.0 login server";
+	directive[4].description  = L"Name of authentication type that corresponds to your login server's first login flavor (e.g. flavor_basic).";
 
 	directive[5].name         = L"AuthTypeName3";
 	directive[5].type         = D_FREE_STRING;
 	directive[5].value        = PBC_AUTHTYPE3;
 	directive[5].defined_in   = defined_in;
-	directive[5].description  = L"Name of authentication type that corresponds to pubcookie plus SecureID authentication";
+	directive[5].description  = L"Name of authentication type that corresponds to your login server's third login flavor.";
 
 	directive[6].name         = L"PUBLIC_dir_name";
 	directive[6].type         = D_FREE_STRING;
 	directive[6].value        = PBC_PUBLIC_NAME;
 	directive[6].defined_in   = defined_in;
-	directive[6].description  = L"If LegacyDirNames names is enabled, a directory with this name will be set to authentication: ";
+	directive[6].description  = L"If LegacyDirNames names is enabled, a directory with this name implicitly sets the ";
 	directive[6].description  += PBC_AUTHTYPE0;
-	directive[6].description  += L" and SetHeaderValues will be enabled.";
+	directive[6].description  += L" authentication type and SetHeaderValues will be enabled.";
 
 	directive[7].name         = L"NETID_dir_name";
 	directive[7].type         = D_FREE_STRING;
 	directive[7].value        = PBC_NETID_NAME;
 	directive[7].defined_in   = defined_in;
-	directive[7].description  = L"If LegacyDirNames names is enabled, a directory with this name will be set to authentication: ";
+	directive[7].description  = L"If LegacyDirNames names is enabled, a directory with this name implicitly sets the ";
 	directive[7].description  += PBC_AUTHTYPE1;
+	directive[7].description  += L" authentication type.";
 
 	directive[8].name         = L"SECURID_dir_name";
 	directive[8].type         = D_FREE_STRING;
 	directive[8].value        = PBC_SECURID_NAME;
 	directive[8].defined_in   = defined_in;
-	directive[8].description  = L"If LegacyDirNames names is enabled, a directory with this name will be set to authentication: ";
+	directive[8].description  = L"If LegacyDirNames names is enabled, a directory with this name implicitly sets the ";
 	directive[8].description  += PBC_AUTHTYPE3;
+	directive[8].description  += L" authentication type.";
 
 	directive[9].name         = L"System_Root";
 	directive[9].type         = D_FREE_STRING;
 	directive[9].value        = L"";
 	directive[9].defined_in   = L"(Program Default)";
-	directive[9].description  = L"Base directory for Pubcookie debug and config files. Leave blank to use the Windows system dir";
+	directive[9].description  = L"Base directory for Pubcookie debug and config files. Leave blank to use the Windows system directory.";
 
 	directive[10].name         = L"ClientLogFormat";
 	directive[10].type         = D_FREE_STRING;
 	directive[10].value        = PBC_CLIENT_LOG_FMT;
 	directive[10].defined_in   = defined_in;
-	directive[10].description  = L"Format to log client username in.  Use %w for Windows user and %p for Pubcookie user";
+	directive[10].description  = L"Format used to log the client username. Use %w for Windows user and %p for Pubcookie user.";
 
 	directive[11].name         = L"WebVarLocation";
 	directive[11].type         = D_FREE_STRING;
 	directive[11].value        = PBC_WEB_VAR_LOCATION;
 	directive[11].defined_in   = defined_in;
-	directive[11].description  = L"Location in Windows registry for Pubcookie directive database.";
+	directive[11].description  = L"Location of the Pubcookie directive database within the Windows registry.";
 
 	directive[12].name         = L"Enterprise_Domain";
 	directive[12].type         = D_FREE_STRING;
 	directive[12].value        = PBC_ENTRPRS_DOMAIN;
 	directive[12].defined_in   = defined_in;
-	directive[12].description  = L"Domain for scoping granting request cookie";
+	directive[12].description  = L"Domain for scoping granting request cookie.";
 
 	directive[13].name         = L"Default_App_Name";
 	directive[13].type         = D_FREE_STRING;
 	directive[13].value        = PBC_DEFAULT_APP_NAME;
 	directive[13].defined_in   = defined_in;
-	directive[13].description  = L"Name to assign if application name cannot be determined, e.g. request to /";
+	directive[13].description  = L"Name to assign if application name cannot be determined (e.g. on a request to /).";
 
 	directive[14].name         = L"Ignore_Poll";
 	directive[14].type         = D_BOUND_INT;
 	directive[14].value        = _itow(PBC_IGNORE_POLL,dbuffer,10);
 	directive[14].defined_in   = defined_in;
-	directive[14].description  = L"Set to 1 to ignore Network Dispatcher \"/\" polls";
+	directive[14].description  = L"Set to 1 to ignore Network Dispatcher \"/\" polls.";
 	directive[14].bound_val[0] = L"0";
 	directive[14].bound_val[1] = L"1";
 
@@ -107,7 +109,7 @@
 	directive[15].type         = D_BOUND_INT;
 	directive[15].value        = _itow(PBC_LEGACY_DIR_NAMES,dbuffer,10);
 	directive[15].defined_in   = defined_in;
-	directive[15].description  = L"Support for legacy directory names. 1=On, 0=Off";
+	directive[15].description  = L"Set to 1 to support legacy directory names.";
 	directive[15].bound_val[0] = L"0";
 	directive[15].bound_val[1] = L"1";
 
