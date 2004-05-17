fd3937396b44a530e166c25dd04b68b077e4c13f
ryanc <ryanc@uw.edu>
2004-05-17 06:38:46 +0000

    Make that version 3.1.0
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1381 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/Win32/PubCookieFilter.h
+++ src/Win32/PubCookieFilter.h
@@ -4,10 +4,10 @@
 //
 
 //
-//  $Id: PubCookieFilter.h,v 1.34 2004-05-17 06:25:37 ryanc Exp $
+//  $Id: PubCookieFilter.h,v 1.35 2004-05-17 06:38:46 ryanc Exp $
 //
 
-#define Pubcookie_Version "Pubcookie ISAPI Filter, 3.1.1"
+#define Pubcookie_Version "Pubcookie ISAPI Filter, 3.1.0"
 
 #define MAX_INSTANCE_ID 25
 #define MAX_REG_BUFF 2048 /* Using a fixed size saves a registy lookup 
