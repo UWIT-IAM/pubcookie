fcbc99fd33b80740c7d5ddece6642795e02ac348
Nathan Dors <dors@uw.edu>
2005-05-13 22:11:16 +0000

    bumped version numbers to 3.2.1 pre-beta2
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1623 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/Win32/PubCookieFilter.h
+++ src/Win32/PubCookieFilter.h
@@ -16,10 +16,10 @@
 //
 
 //
-//  $Id: PubCookieFilter.h,v 1.48 2005-05-13 15:43:13 suh Exp $
+//  $Id: PubCookieFilter.h,v 1.49 2005-05-13 22:11:16 dors Exp $
 //
 
-#define Pubcookie_Version "Pubcookie ISAPI Filter, 3.2.1 beta1"
+#define Pubcookie_Version "Pubcookie ISAPI Filter, 3.2.1 pre-beta2"
 
 #define MAX_INSTANCE_ID 64
 #define MAX_REG_BUFF 2048 /* Using a fixed size saves a registy lookup 
