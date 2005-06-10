d62fe79b9c78e9e17bbaec7f789a6ca442a2f968
Steve Mats Mats <willey@uw.edu>
2005-06-10 17:42:03 +0000

    patch to not pass args on command line, from David Houlder @ Australian National University
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1643 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/verify_fork.c
+++ src/verify_fork.c
@@ -17,22 +17,35 @@
  */
 
 /** @file verify_fork.c
+
  *  Verifier that forks something and uses it to authenticate
  *
- * A verifier which launches another application with 2 
- * arguements, userid and password. The called program will then 
- * set a non-zero exit code if authentication fails. The called program will 
- * exit with 0 status if all is OK.
+ * A verifier which launches another program which must read 4 string
+ * arguments - userid, password, service and realm - from stdin. Each
+ * string is terminated by a null character, \0. The called program
+ * must then return a non-zero exit code if authentication fails. The
+ * called program must exit with 0 status if all is OK.
+
  *
  * To use verify_fork:
  * a) In your config:  'basic_verifier: verify_fork'
- * b) The application to run is specified by a parameter called "fork_exe", 
+ * b) The application to run is specified by a parameter called "verify_exe", 
  * for example:
- * fork_exe: /usr/local/pubcookie/runme.pl
+ * verify_exe: /usr/local/pubcookie/readauth.py
  *
+ * ...where readauth.py could be:
+ *  #!/usr/bin/env python
+ *  import sys
+ *  import myauth
+ *  user, pass, serv, realm=sys.stdin.read().split('\0')
+ *  status=myauth.lookup(username, password, serv, realm)
+ *  sys.exit(status)
+ *  
  * From Tim Funk <funkman@joedog.org> 18-Sept-2003
+ * Modified 4-April-2005: Fixed security issue - read user and
+ * password from stdin rather passing as args. david.houlder@anu.edu.au
  *
- * $Id: verify_fork.c,v 1.8 2005-05-18 21:38:53 willey Exp $
+ * $Id: verify_fork.c,v 1.9 2005-06-10 17:42:03 willey Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -70,14 +83,16 @@ int verify_fork_v (pool * p,
     pid_t pid;
     int status, died;
     char *fork_exe;
+    int stdin_pipe[2];
+
     if (errstr)
         *errstr = NULL;
     if (creds)
         *creds = NULL;
     pbc_log_activity (p, PBC_LOG_DEBUG_OUTPUT, "verify_fork: enter");
-    fork_exe = (char *) libpbc_config_getstring (p, "fork_exe", NULL);
+    fork_exe = (char *) libpbc_config_getstring (p, "verify_exe", "");
     pbc_log_activity (p, PBC_LOG_DEBUG_VERBOSE,
-                      "verify_fork: fork_exe=%s", fork_exe);
+                      "verify_fork: verify_exe=%s", fork_exe);
     if (!userid) {
         *errstr = "no userid to verify";
         return -1;
@@ -86,8 +101,18 @@ int verify_fork_v (pool * p,
         *errstr = "no password to verify";
         return -1;
     }
+    if (!service)
+        service = "";
+    if (!user_realm)
+        user_realm = "";
+
+    if (-1 == pipe (stdin_pipe)) {
+        *errstr = "could not create pipe to child process";
+        return -1;
+    }
     pbc_log_activity (p, PBC_LOG_DEBUG_OUTPUT,
                       "verify_fork: about to fork");
+
     switch (pid = fork ()) {
     case -1:
         pbc_log_activity (p, PBC_LOG_ERROR, "verify_fork: Couldn't fork");
@@ -96,15 +121,34 @@ int verify_fork_v (pool * p,
     case 0:
         pbc_log_activity (p, PBC_LOG_DEBUG_OUTPUT,
                           "verify_fork: about to execl");
-        execl (fork_exe, fork_exe, userid, passwd, NULL);
+        close (0);
+        if (0 == dup (stdin_pipe[0]) &&
+            0 == close (stdin_pipe[0]) && 0 == close (stdin_pipe[1])) {
+            execl (fork_exe, fork_exe, NULL);
 
-        /* Should not occur since execl doesn't return */
-        pbc_log_activity (p, PBC_LOG_ERROR,
-                          "verify_fork: can't exec, errno=%d", errno);
+            /* Should not occur since execl doesn't return */
+            pbc_log_activity (p, PBC_LOG_ERROR,
+                              "verify_fork: can't exec, errno=%d", errno);
+        } else
+            pbc_log_activity (p, PBC_LOG_ERROR,
+                              "verify_fork: can't set up pipe, errno=%d",
+                              errno);
         exit (-1);
     default:
         pbc_log_activity (p, PBC_LOG_DEBUG_OUTPUT,
                           "verify_fork: about to wait");
+        close (stdin_pipe[0]);
+        /* write strlen()+1 to write the \0. O_NONBLOCK is clear so we
+           get either a full write or -1 returned */
+        if (-1 == write (stdin_pipe[1], userid, strlen (userid) + 1) ||
+            -1 == write (stdin_pipe[1], passwd, strlen (passwd) + 1) ||
+            -1 == write (stdin_pipe[1], service, strlen (service) + 1) ||
+            -1 == write (stdin_pipe[1], user_realm,
+                         strlen (user_realm) + 1))
+            pbc_log_activity (p, PBC_LOG_ERROR,
+                              "verify_fork: Write to child failed, errno=%d",
+                              errno);
+        close (stdin_pipe[1]);
         if (-1 == waitpid (pid, &status, 0)) {
             pbc_log_activity (p, PBC_LOG_ERROR,
                               "verify_fork: Wait for child failed");
