2beb062fca28bfd3ae974edd5ff13c04e052ae9d
Nathan Dors <dors@uw.edu>
2005-12-08 00:18:55 +0000

    regenerated redux
    git-svn-id: https://svn.cac.washington.edu/svn/pubcookie/trunk@1784 b9756b1c-b508-4e7c-84e7-73dc1d0553b6

--- src/pbc_version.h
+++ src/pbc_version.h
@@ -64,6 +64,6 @@
  * by configure when it is run again!
  *************************************************************************** */
 
-#define PBC_VERSION_STRING "3.3.0 pre-beta2"
+#define PBC_VERSION_STRING "3.3.0 beta2"
 
 #endif /* !PUBCOOKIE_VERSION */
