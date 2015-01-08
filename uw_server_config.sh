0865bfd7006490f88a99fe6e81c2d88e36ea93dd
Jim Fox <fox@uw.edu>
2015-01-08 11:29:46 -0800

    configure script for UW

---
+++ uw_server_config.sh
@@ -0,0 +1,13 @@
+./configure \
+   --enable-login \
+   --enable-krb5 \
+   --disable-apache \
+   --with-ssl=/usr/lib64 \
+   --with-ezs=yes \
+   --with-lsc=no \
+   --with-mango-inc-dir=/tulsa/include \
+   --with-mango-lib-dir=/tulsa/lib/rhel6 \
+   --with-audit-log=LOG_LOCAL3 \
+   --with-general-log=LOG_LOCAL3 \
+   --enable-uwsecurid
+
